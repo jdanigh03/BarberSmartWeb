a362f95f0f75a2039f86537a8727fd7e427a1314
jdanigh03 <danielguevarahoyos03@gmail.com>
2025-05-28 21:27:53 -0400

    integrando base

--- src/pages/PaymentsPage.jsx
+++ src/pages/PaymentsPage.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 
 const PaymentsPage = () => {
    const dummyPayments = [
-    { id: 1, client: 'Juan Rodríguez', amount: 25.00, date: '2024-07-28', status: 'Pagado', method: 'Tarjeta' },
-    { id: 2, client: 'Ana Gómez', amount: 30.00, date: '2024-07-29', status: 'Pendiente', method: '-' },
+    { id: 1, client: 'Juan Rodríguez', amount: 25.00, date: '2024-07-28', status: 'Realizado', method: 'Tarjeta' },
+    { id: 2, client: 'Ana Gómez', amount: 30.00, date: '2024-07-29', status: 'Pendiente', method: 'E  fectivo' },
   ];
   return (
     <div>
       <h1>Gestión de Pagos</h1>
       <p>Revisa el cumplimiento de los pagos de los servicios.</p>
       <div style={{ marginTop: '20px', border: '1px solid var(--color-border)', padding: '15px', borderRadius: '8px', backgroundColor: 'var(--color-surface)' }}>
-        <h2>Listado de Pagos (Ejemplo)</h2>
+        <h2>Historial de pagos</h2>
         {dummyPayments.map(payment => (
           <div key={payment.id} style={{ marginBottom: '10px', paddingBottom: '10px', borderBottom: '1px solid #eee' }}>
             Cliente: <strong>{payment.client}</strong> - Monto: ${payment.amount.toFixed(2)}
